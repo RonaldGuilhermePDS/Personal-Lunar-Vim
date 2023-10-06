8331dfdcb700902c9893e88e3772a4f393b36f9d
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-10-06 15:35:15 -0300

    chore: add hop plugin

--- config.lua
+++ config.lua
@@ -3,22 +3,29 @@
 -- Plugins
 lvim.plugins = {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestions = { enabled = false },
-        panel = { enabled = false },
-      })
-    end,
-  },
-  {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
   },
   {"jose-elias-alvarez/typescript.nvim"},
   {"lunarvim/lunar.nvim"},
-  {"wakatime/vim-wakatime"}
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+    end,
+  },
+  {"wakatime/vim-wakatime"},
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+    end,
+  },
 }
 
