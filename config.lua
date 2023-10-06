dcda7c0a28214f6657f42a47eff9a9fa65cc35b2
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-10-06 15:22:12 -0300

    chore: add copilot plugin

--- config.lua
+++ config.lua
@@ -2,8 +2,23 @@
 
 -- Plugins
 lvim.plugins = {
-  {"folke/trouble.nvim"},
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestions = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
   {"jose-elias-alvarez/typescript.nvim"},
+  {"lunarvim/lunar.nvim"},
   {"wakatime/vim-wakatime"}
 }
 
