1ec8718a633d6039eb8ad4a3bbc0b35af980d62b
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-10-07 16:07:04 -0300

    chore: add tree sitter packages

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,6 +1,21 @@
 -- General
-lvim.format_on_save = {
-  enabled = true,
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "json",
+  "yaml",
+  "html",
+  "css",
+  "javascript",
+  "tsx",
+  "typescript",
+  "lua",
+  "erlang",
+  "eex",
+  "heex",
+  "elixir",
+  "hcl"
 }
+lvim.format_on_save = true
+lvim.log.level = "warn"
 
 vim.cmd("set history=0")
