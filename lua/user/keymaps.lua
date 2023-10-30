59745f209d0ee2b625b0e51c4f64cf07dbfa8b79
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-10-29 23:21:09 -0300

    chore: set shift + h or shift + l to change buffer file keymap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,3 +1,5 @@
 lvim.leader = "space"
 
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
