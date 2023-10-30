8aace9473792b1b580cc86d6bbd5d91a63e35981
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-10-29 23:44:44 -0300

    chore: add find file and find reference telescope keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,6 +1,19 @@
+local kind = require('user.kind')
+
 lvim.leader = "space"
 
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
 vim.keymap.set('n', 'gn', ":tabe %<CR>")
+
+lvim.lsp.buffer_mappings.normal_mode["ff"] = {
+  ":Telescope find_files<cr>",
+  kind.cmp_kind.Reference .. " Find Files"
+}
+
+lvim.lsp.buffer_mappings.normal_mode["fr"] = {
+  ":Telescope live_grep<cr>",
+  kind.cmp_kind.Reference .. " Find References"
+}
