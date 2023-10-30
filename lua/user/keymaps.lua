3d4f1343b4cc9578c258a048b3e49d5012d865cd
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-10-29 23:26:33 -0300

    chore: set gn on normal mode to generate new tab

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -3,3 +3,4 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+vim.keymap.set('n', 'gn', ":tabe %<CR>")
